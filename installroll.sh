b77ca58b204018f214a7d6149a10ce23129de2c4
drlellinger <39723641+drlellinger@users.noreply.github.com>
2019-03-24 21:15:23 +0100

    Create installroll.sh

---
+++ installroll.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "alias rickroll='curl -L https://raw.githubusercontent.com/drlellinger/rickrollrc/master/roll.sh | bash'" >> ~/.bash_profile
+echo "alias rickroll='curl -L https://raw.githubusercontent.com/drlellinger/rickrollrc/master/roll.sh | bash'" >> ~/.bashrc

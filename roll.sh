fbd8ef4f52c42f4b95d0f776f639a9476d3b30f9
drlellinger <39723641+drlellinger@users.noreply.github.com>
2019-03-24 21:23:21 +0100

    Update roll.sh

--- roll.sh
+++ roll.sh
@@ -34,7 +34,8 @@ for arg in "$@"; do
     usage && exit
   elif [[ "$arg" == "inject" ]]; then
     echo -en "${red}[Inject] "
-    echo $NEVER_GONNA >> $MAKE_YOU_CRY
+    echo "alias rickroll='curl -L https://raw.githubusercontent.com/drlellinger/rickrollrc/master/roll.sh | bash'" >> ~/.bash_profile
+    echo "alias rickroll='curl -L https://raw.githubusercontent.com/drlellinger/rickrollrc/master/roll.sh | bash'" >> ~/.bashrc
     echo -e "${green}Appended to $MAKE_YOU_CRY. <3"
     echo -en "${yell}If you've astley overdosed, "
     echo -e "delete the line ${purp}\"$NEVER_GONNA\"${yell}."

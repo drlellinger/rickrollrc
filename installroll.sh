3986abb2b52bc0f882a44dd950a8b43d3343adaf
drlellinger <39723641+drlellinger@users.noreply.github.com>
2019-03-24 21:22:45 +0100

    Delete installroll.sh

--- installroll.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "alias rickroll='curl -L https://raw.githubusercontent.com/drlellinger/rickrollrc/master/roll.sh | bash'" >> ~/.bash_profile
-echo "alias rickroll='curl -L https://raw.githubusercontent.com/drlellinger/rickrollrc/master/roll.sh | bash'" >> ~/.bashrc
